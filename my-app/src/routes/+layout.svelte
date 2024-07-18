a8aa8d3675f5cc8347f7bbf1c7b1cbd6ddeef544
Morgan <morgan.rapaport@gmail.comm>
2024-07-18 16:25:40 +0200

    Added both the background vectors

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -1,10 +1,38 @@
 <script>
   import Navbar from '../components/Navbar.svelte';
-  import '../styles/app.css'; // Import Tailwind CSS
+  import '../styles/app.css'; 
 </script>
 
-<Navbar />
-<main class="p-4">
-  <!-- Your main content goes here -->
-</main>
-<slot></slot>
+<style>
+  .background-svg {
+    position: absolute;
+    z-index: -1; 
+    display: block;
+  }
+
+  .svg-top-left-overflow {
+    top: -288px; 
+    left: 0px; 
+    width: 1946px; 
+    height: 1677px; 
+  }
+
+  .svg-bottom-right-overflow {
+    top: 1020px; /* Adjust this value to move the SVG vertically */
+    left: 0px; /* Adjust this value to move the SVG horizontally */
+    width: 1512px; /* Set the width */
+    height: 3548px; /* Set the height */
+  }
+</style>
+
+<div class="relative">
+  <!-- SVG Backgrounds -->
+  <img src="/src/assets/vector1.svg" alt="Background Vector 1" class="background-svg svg-top-left-overflow" />
+  <img src="/src/assets/vector2.svg" alt="Background Vector 2" class="background-svg svg-bottom-right-overflow" />
+
+  <!-- Page Content -->
+  <Navbar />
+  <main class="p-4">
+    <slot></slot>
+  </main>
+</div>
