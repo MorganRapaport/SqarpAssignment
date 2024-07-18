be6931a879d860cdca7f443945f29620fc12ccd5
Morgan <morgan.rapaport@gmail.comm>
2024-07-18 12:11:27 +0200

    tailwind.css set up

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,3 +1,4 @@
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p> hello </p> 
+<h1 class="py-8 px-4 border border-indigo-900 shadow-lg">Hello {name}!</h1>
