a34491d8813e86c3ffca94bcd423880136208218
Morgan <morgan.rapaport@gmail.comm>
2024-07-22 21:00:00 +0200

    Remade the background, deleted vectors and used background image instead.

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -4,9 +4,7 @@
 </script>
 
 <div class="relative">
-  <img src="/src/assets/vector1.svg" alt="Background Vector 1" class="absolute -z-10 block top-[-12rem] left-[0.5rem] w-[121.625rem] h-[104.8125rem]" />
-  <img src="/src/assets/vector2.svg" alt="Background Vector 2" class="absolute -z-10 block top-[70rem] left-0 w-[94.5rem] h-[221.75rem]" />
-
+  <img src="/src/assets/background.png" alt="Background" class="absolute -z-10 block" />
   <Navbar />
   <main class="p-4">
     <slot></slot>
