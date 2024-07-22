0e568c53bde1ce1cea013b66992955fca276a47b
Morgan <morgan.rapaport@gmail.comm>
2024-07-22 18:53:29 +0200

    Fixed paragraph so its correct text for each card

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -34,7 +34,7 @@
     imageSrc="/src/assets/computer.png"
     header="E-optimization"
     subheader="SOLUTIONS"
-    paragraph="Business that achieve digital data excellence see +50% online revenue growth. E-optimization is our method for getting there in < 4 weeks without major investments."
+    paragraph="SQARP eCom services is a suite of tools for managing product information and digital assets that help companies excel in digital retail."
     buttonText="Explore"
     buttonClick={() => console.log("Explore E-optimization clicked")}
 		reverse={true} 
@@ -44,16 +44,16 @@
 	imageSrc="/src/assets/boat.png"
 	header="Data Distribution"
     subheader="SOLUTIONS"
-    paragraph="Automate data delivery to customers to reduce admin cost and time-to-market by 90%, while growing partner sales."
-    buttonText="Explore"
+    paragraph="Automate product data onboarding from all your suppliers to scale your catalogue endlessly while significantly improving customer experience."
+		buttonText="Explore"
     buttonClick={() => console.log("Explore Data Distribution clicked")}
   />
 	<CardComponent 
     imageSrc="/src/assets/blue.png"
     header="E-optimization"
     subheader="SOLUTIONS"
-    paragraph="Business that achieve digital data excellence see +50% online revenue growth. E-optimization is our method for getting there in < 4 weeks without major investments."
-    buttonText="Explore"
+    paragraph="Easy-to-use library of AI-tools for product information professionals, increasing merchandising productivity 10x."
+		buttonText="Explore"
     buttonClick={() => console.log("Explore E-optimization clicked")}
 		reverse={true} 
   />
